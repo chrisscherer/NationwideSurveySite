6f7918661c59baaf3f60a0f037a89eacda74c4f6
chrisscherer <chrisscherer90@gmail.com>
2015-04-20 10:01:39 -0400

    test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SurveySite::Application.config.secret_key_base = '3194c063cb7cefb2288ca919843bf7f173f787cb28f775765386a4d8654eeffc3b2547f07609b7484f83c3c8b2f842d246fcc148c6c126ff432858e8565ad87a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SurveySite::Application.config.session_store :cookie_store, key: '_Survey_Site_session'
